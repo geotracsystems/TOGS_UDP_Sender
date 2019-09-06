156886cb087e046d9730a9393067a33b43a5a3e6
Syed Raheem <Syed_Raheem@Trimble.com>
2019-09-06 11:44:35 -0600

    add support for CSV file for Bluetrees

--- test_bluetree_msg.py
+++ test_bluetree_msg.py
@@ -1,4 +1,6 @@
 import sys
+import csv
+from time import sleep
 from string import Template
 from utils.bep_helper import *
 from utils.send_udp import send_udp
@@ -16,9 +18,21 @@ bep_body_template = Template(
 )
 
 
+def make_bep_message(esn, seqno, eventid, latitude, longitude, speed):
+    bep_body = bep_body_template.substitute(gp1=nmea_sentence(latitude, longitude, speed))
+    full_length_hex = hex(int(len(bep_body) / 3 + 24)).replace('x', '').upper().zfill(4)
+    length_format = f'{full_length_hex[2:4]} {full_length_hex[0:2]}'
+
+    bep_header = bep_header_template.substitute(imei=bep_esn(esn), length=length_format, seqno=bep_seq(seqno),
+                                                eventid=bep_event(eventid))
+
+    bep_message = message_frame_start + bep_header + bep_body + message_frame_end
+    return bep_message
+
+
 seqno = 1
 
-if len(sys.argv) != 9:
+if len(sys.argv) != 9 and len(sys.argv) != 5:
     print("Invalid arguments")
     print(len(sys.argv))
     print("Usage 1:\n", sys.argv[0], "<imei> <host> <port> <filename>")
@@ -35,18 +49,30 @@ if sys.argv[4] == 'single':
     longitude = sys.argv[6]
     speed = int(sys.argv[7])
     eventid = int(sys.argv[8])
-
-
-bep_body = bep_body_template.substitute(gp1=nmea_sentence(latitude, longitude, speed))
-full_length_hex = hex(int(len(bep_body)/3 + 24)).replace('x', '').upper().zfill(4)
-length_format = f'{full_length_hex[2:4]} {full_length_hex[0:2]}'
-
-bep_header = bep_header_template.substitute(imei=bep_esn(esn), length=length_format, seqno=bep_seq(seqno),
-                                            eventid=bep_event(eventid))
-
-
-bep_message = message_frame_start + bep_header + bep_body + message_frame_end
-print(bep_message)
-print(len(bep_message)/3)
-
-response = send_udp(host, port, bep_message)
+    bep_message = make_bep_message(esn, seqno, eventid, latitude, longitude, speed)
+    print("Sending single line\n", bep_message)
+    response = send_udp(host, port, bep_message)
+    print("Received Ack\n", response)
+elif sys.argv[4] != 'single' and len(sys.argv) == 5:
+    filename = sys.argv[4]
+    column_names = []
+    with open(filename) as csv_file:
+        lc = 0
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if lc == 0:
+                for item in row:
+                    column_names.append(item)
+                lc = lc + 1
+            else:
+                latitude = row[0]
+                longitude = row[1]
+                speed = int(row[2])
+                eventid = int(row[3])
+                bep_message = make_bep_message(esn, seqno, eventid, latitude, longitude, speed)
+                print(f"Sending line {lc} from {filename}\n{bep_message}")
+                response = send_udp(host, port, bep_message)
+                print("Received Ack\n", response)
+                lc = lc + 1
+            seqno = seqno + 1
+            sleep(1)
