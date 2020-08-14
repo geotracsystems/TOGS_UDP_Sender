7ddcfc2d6399595406dd0ae9f04ab4ddf36b430f
Syed Raheem <sraheem@internal.geotracinternational.com>
2020-08-14 10:27:09 -0600

    Making it CI friendly

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def get_version():
+    f = open('versions.txt')
+    for line in f.read().splitlines():
+        if line.startswith('__version__'):
+            delim = "'"
+            return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="TOGS UDP Sender",
+    version=get_version(),
+    scripts=["togs_udp_sender.py"],
+    author="Syed Raheem",
+    author_email="syed_raheem@trimble.com",
+    description="Test tool for sending various TOGS messages to the backend over UDP."
+)
